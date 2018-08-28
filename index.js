16839a29b76f6c08a33200d327050a2fa90d995c
Brian <briankn8@gmail.com>
2018-08-27 18:24:13 -0600

    Added pruning, avatars, and kicking

--- index.js
+++ index.js
@@ -1,40 +1,64 @@
 const { prefix } = require('./config.json');
-
-// require the discord.js module
 const Discord = require('discord.js');
-
 require('dotenv').config();
 
-
-// create a new Discord client
 const client = new Discord.Client();
-
-// when the client is ready, run this code
-// this event will trigger whenever your bot:
-// - finishes logging in
-// - reconnects after disconnecting
 
 client.on('ready', () => {
     console.log('Ready!');
 });
 
-// login to Discord with your app's token
 client.login(process.env.TOKEN);
 
 client.on('message', message => {
-	if (message.content.startsWith(`${prefix}ping`)) {
-    	message.channel.send('Pong.');
-	}
-	else if (message.content.startsWith(`${prefix}beep`)) {
-	    message.channel.send('Boop.');
-	}
-	else if (message.content.startsWith(`${prefix}server`)) {
-		message.channel.send(`This server's name is: ${message.guild.name} 
-			\n Total members: ${message.guild.memberCount}
-			\n This server was created at: ${message.guild.createdAt}`);
-	}
-	else if (message.content.startsWith(`${prefix}user-info`)) {
-		message.channel.send(`Your username: ${message.author.username}`);
-	}
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
 
+	const args = message.content.slice(prefix.length).split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	if (command === 'ping') {
+		if (args[0] === 'pong') {
+			message.channel.send('Wow you already said pong for me!')
+		}
+		else {
+			message.channel.send('Pong.');
+		}
+	}
+	else if (command === 'kick') {
+		if (!message.mentions.users.size) {
+    		return message.reply('you need to tag a user in order to kick them!');
+    	}
+    	const taggedUser = message.mentions.users.first();
+		message.channel.send(`You want to kick: ${taggedUser}`);
+	}	
+	else if (command === 'avatar') {
+		if (!message.mentions.users.size) {
+    		return message.channel.send(`Your avatar: ${message.author.displayAvatarURL}`);
+    	}
+    	const avatarList = message.mentions.users.map(user => {
+        	return `${user.username}'s avatar: ${user.displayAvatarURL}`;
+    	});
+
+	    // send the entire array of strings as a message
+	    // by default, discord.js will `.join()` the array with `\n`
+	    message.channel.send(avatarList);
+	}
+	else if (command === 'prune') {
+		const amount = parseInt(args[0]);
+		if (isNaN(amount)) {
+			return message.reply('that doesn\'t seem to be a valid number.');
+		}
+		else if (amount < 2 || amount > 100) {
+			return message.reply('you need to input a number between 2 and 100.');
+		}
+		message.channel.bulkDelete(amount, true).catch(err => {
+    		console.error(err);
+    		if (err.message === 'Missing Permissions') {
+    			message.reply(`I don't have permission to do that in here!`);
+    		}
+    		else {
+	    		message.reply('There was an error trying to prune messages in this channel!');
+    		}
+		});	
+	}	
 });
