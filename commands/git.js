45e115c788f1baf6a19872d594e6ec39fc021f0c
Brian <briankn8@gmail.com>
2018-08-30 16:29:35 -0600

    Added guide

--- commands/git.js
+++ commands/git.js
@@ -4,7 +4,7 @@ module.exports = {
 	name: 'git',
 	description: 'Github commands',
 	aliases: ['github'],
-	usage: '<zen> or <user> <username> or <issues> <user> <repo>',
+	usage: '<zen> or <user> <username> or <issues> <user> <repo> or <repo> <user> <repo-name>',
 	args: true,
 	async execute(message, args) {
 		if (args[0] == 'zen') {
