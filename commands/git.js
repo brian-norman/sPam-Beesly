cc00fef799ceafa1d311341251b43332695cb4bc
Brian <briankn8@gmail.com>
2018-08-30 16:48:30 -0600

    Added listing repos for users

--- commands/git.js
+++ commands/git.js
@@ -10,7 +10,6 @@ module.exports = {
 		if (args[0] == 'zen') {
 		    const { body } = await snekfetch.get('https://api.github.com/zen');
 			message.channel.send(`GitHub Zen Principle: ${body}`);
-
 		}
 		else if (args[0] == 'user') {
 			if (args.length != 2) {
@@ -25,14 +24,33 @@ module.exports = {
 			}
 			const { body } = await snekfetch.get(`https://api.github.com/repos/${args[1]}/${args[2]}/issues`);
 			message.channel.send(`Issues for ${args[2]}: \n`);
-			for (var i = body.length - 1; i >= 0; i--) {
+			for (var i = 0; i < body.length; i++) {
 				message.channel.send(`Issue ${i+1}: ${body[i].title} - ${body[i].body}`);
 			}
 		}
 		else if (args[0] == 'repo') {
-			if (args.length) {}
-			const { body } = await snekfetch.get(`https://api.github.com/repos/${args[1]}/${args[2]}`);
-			message.channel.send(`Link to ${args[2]}: ${body.html_url}`)
+			if (args.length == 2) {
+				const { body } = await snekfetch.get(`https://api.github.com/users/${args[1]}/repos`);
+				if (body.length > 3) {
+					for (var i = 0; i < 3; i++) {
+						message.channel.send(`Repo ${i+1}: ${body[i].html_url}`);
+					}
+					message.channel.send(`More available at: ${body[i].owner.html_url}`);
+				}
+				else {
+					for (var i = 0; i < body.length; i++) {
+						message.channel.send(`Repo ${i+1}: ${body[i].html_url}`);
+					}
+				}
+			}
+			else if (args.length == 3) {
+				const { body } = await snekfetch.get(`https://api.github.com/repos/${args[1]}/${args[2]}`);
+				message.channel.send(`Link to ${args[2]}: ${body.html_url}`)
+			}
+			else {
+				message.channel.send(`Woops`);
+			}
+			
 		}
 	},
 };
